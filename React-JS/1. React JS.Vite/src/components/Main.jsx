aa03fa37a648cf46be2236238eaf313a9e02dd31
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-03-05 22:58:15 +0200

    Add Introduction to React and JSX

---
+++ React-JS/1. React JS.Vite/src/components/Main.jsx
@@ -0,0 +1,8 @@
+const Main = () =>{
+    return(
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam aliquid, officia consequuntur doloribus nisi minus at perspiciatis! Cum voluptas dicta architecto id tempora! Sit illo molestias similique minima qui sint?</p>
+
+    )
+}
+
+export default Main
