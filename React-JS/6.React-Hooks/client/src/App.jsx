f862fcd3be4abece2b216c5504977b0b4d07bec9
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-03-14 16:58:02 +0200

    Create useFetch hook

--- React-JS/6.React-Hooks/client/src/App.jsx
+++ React-JS/6.React-Hooks/client/src/App.jsx
@@ -1,10 +1,17 @@
-import Chat from "./components/Chat"
+import useFetch from "./hooks/useFetch"
 
+import Chat from "./components/Chat"
+import { Spin } from "antd";
+
+const url = 'http://localhost:3030/jsonstore/messages'
 function App() {
 
+  const [pending, messages]= useFetch(url);
   return (
     <>
-        <Chat/>
+        {pending
+          ? <Spin />
+          : <Chat messages={messages} />}
     </>
   )
 }
