3b2139cad926b164b19d00caf7acec4da8c14c8c
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-03-17 17:28:17 +0200

    Add AbortController

--- React-JS/6.React-Hooks/client/src/hooks/useFetch.js
+++ React-JS/6.React-Hooks/client/src/hooks/useFetch.js
@@ -3,16 +3,24 @@ import {useState, useEffect}  from 'react'
 export default function useFetch(url, defaultState = {}) {
     const [state, setState] = useState(defaultState);
     const [pending, setPending] = useState(true)
-    useEffect(() =>{
 
+    useEffect(() =>{
+        
         setPending(true);
+        const abortController = new AbortController();
+
         fetch(url)
             .then(res => res.json())
             .then(result =>{
                 setState(Object.values(result));
                 setPending(false)
             })
+
+            return () =>{
+                abortController.abort();
+            }
     },[url])
+
 
     return [pending, state]
 }
